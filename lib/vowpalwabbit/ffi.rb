5c567521a472716daeb5f3dc9857d007e967ee12
Andrew Kane <andrew@chartkick.com>
2019-11-05 00:34:22 -0800

    Added comment [skip ci]

--- lib/vowpalwabbit/ffi.rb
+++ lib/vowpalwabbit/ffi.rb
@@ -11,6 +11,7 @@ module VowpalWabbit
       raise LoadError, "Could not find Vowpal Wabbit"
     end
 
+    # fiddler doesn't like void
     typealias "void", "void*"
 
     typealias "VW_HANDLE", "void*"
